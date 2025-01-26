f8e2d3b25a49b7393ab000e2f28e0b61637fe382
roylee0912 <leeroy0912@gmail.com>
2025-01-26 03:04:39 -0800

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { ReactQueryProvider } from "@/lib/react-query/provider"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,6 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Analytics />
       </body>
     </html>
   )
