513b9779efdd163058673fa0019e7bb9e61494c3
roylee0912 <leeroy0912@gmail.com>
2025-01-29 19:41:47 -0500

    added prompt

--- app/api/generate/solution.ts
+++ app/api/generate/solution.ts
@@ -87,7 +87,7 @@ ${JSON.stringify(problemInfo.test_cases ?? [], null, 2)}`
                 problemInfo.language ?? "python"
               } code generator that only outputs valid ${
                 problemInfo.language ?? "python"
-              } code solutions. You should use a minimal amount of external libraries, and you should be writing code that is legible and the optimal solution in terms of time and space complexity. It is very important that this code is legible and understandable, so add comments next to relevant places in the code that explain what the code does. Absolutely no markdown. Write your answer in the style of a solution to a Leetcode problem. Do not add any comments after the solution explaining how to use the function.` +
+              } code solutions. You should use a minimal amount of external libraries, and you should be writing code that is legible and the optimal solution in terms of time and space complexity. It is very important that this code is legible and understandable, so add comments next to relevant places in the code that explain what the code does. Absolutely no markdown. Write your answer in the style of a solution to a Leetcode problem and expect that the difficulty will be that of a medium-level Leetcode problem, making sure to go for the solution that is most optimal. Do not add any comments after the solution explaining how to use the function.` +
               promptContent
           }
         ]
