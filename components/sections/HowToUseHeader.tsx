26924bd15b0ef31baf0d01d6d5e62e8d3aa1aabc
roylee0912 <leeroy0912@gmail.com>
2025-02-08 00:17:27 -0500

    add redirect

--- components/sections/HowToUseHeader.tsx
+++ components/sections/HowToUseHeader.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from "react"
 import { motion } from "framer-motion"
+import Link from "next/link"
 
 export function HowToUseHeader() {
   return (
@@ -17,6 +18,24 @@ export function HowToUseHeader() {
       >
         How to Use Interview Coder
       </motion.h2>
+      <motion.h3
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.5,
+          duration: 0.8,
+          ease: "easeInOut"
+        }}
+        className="text-center text-lg leading-8 text-[#999999]"
+      >
+        For an in-depth tutorial on setting it up, visit our{" "}
+        <Link
+          href="/help"
+          className="underline hover:text-gray-700  transition-colors"
+        >
+          help center
+        </Link>
+      </motion.h3>
     </div>
   )
 }
