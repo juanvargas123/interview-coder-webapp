c3f7da04f715388209952e59120784290b687649
Neel Shanmugam <neel.shanmugam@gmail.com>
2025-03-02 21:19:24 -0500

    potential clicks fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,11 +24,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script id="first-promoter-script" strategy="afterInteractive">
-          {`(function(w){w.fpr=w.fpr||function(){w.fpr.q = w.fpr.q||[];w.fpr.q[arguments[0]=='set'?'unshift':'push'](arguments);};})(window);
-          fpr("init", {cid:"4z8yt1oa"}); fpr("click");`}
-        </Script>
-        <Script src="https://cdn.firstpromoter.com/fpr.js" strategy="afterInteractive" />
+        {/* First Promoter tracking script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w){w.fpr=w.fpr||function(){w.fpr.q = w.fpr.q||[];w.fpr.q[arguments[0]=='set'?'unshift':'push'](arguments);};})(window);
+              fpr("init", {cid:"4z8yt1oa"}); fpr("click");
+            `
+          }}
+        />
+        <script src="https://cdn.firstpromoter.com/fpr.js" async></script>
       </head>
       <body className={inter.className}>
         <ReactQueryProvider>
